14e2d659f1941fc15461c81d04d60914ce7b9eee
Rafael Drigo <rafael.drigo@superior-ind.com>
2024-07-31 14:02:58 -0300

    Start of studies

---
+++ module-one/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // Arithmetic operators
+        System.out.println("\nThe most common arithmetic operators in Java are:");
+        System.out.println("+ --> Addition");
+        System.out.println("- --> Subtraction");
+        System.out.println("* --> Multiplication");
+        System.out.println("/ --> Division");
+        System.out.println("& --> Remainder of division (\"mod\")");
+
+        // Variables
+        System.out.println("\nHow can I define a variable?");
+        System.out.println("<type> <name> = <variable value (optional)>, EX: String username = \"Rafael Drigo\";");
+        String username = "Rafael Drigo";
+        System.out.printf("The username in the variable is %s%n", username);
+    }
+}
